11385417712c8cb363f27e20002e1808e5c7cde1
Maria Ryumina <maria.ryumin@gmail.com>
2023-10-12 13:20:24 +0300

    fix: ошибки тестов

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
+app.use('/*', (req, res, next) => {
   res.status(404).send({ message: 'Страница не найдена' });
   next();
 });
