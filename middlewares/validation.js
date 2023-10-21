27382904e56058fd7fdbc834dcc0a8d4cd4a4d98
Maria Ryumina <maria.ryumin@gmail.com>
2023-10-22 00:00:15 +0300

    feat: обработка ошибок, валидация

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -47,7 +47,7 @@ module.exports = {
   }),
   likeCardValidation: celebrate({
     body: Joi.object().keys({
-      userId: Joi.string().hex().length(24),
+      cardId: Joi.string().hex().length(24).required(),
     }),
   }),
 };
